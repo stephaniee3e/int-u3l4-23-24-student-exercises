1b51afabdd6d14e0d4d0a79f4470308a71fb60d7
stephaniee3e <158521537+stephaniee3e@users.noreply.github.com>
2024-04-19 11:55:15 +0000

    complete

--- domManipulation.js
+++ domManipulation.js
@@ -23,23 +23,28 @@ let image = document.querySelector('.image');
 
 contentButton.addEventListener('click', function() {
 
-
-
-
+myName.innerHTML="My name is Stephanie!";
+myHobby.innerHTML="I like to do arts and crafts";
+myLocation.innerHTML="I live in Queens";
 
 
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
+image1Button.addEventListener ("click",function(){
+  image.src= "https://m.media-amazon.com/images/S/pv-target-images/ce8c15666ad504500a868c77165d3f1f9cfecda3d7970d27ae134cfff2a1d85f.jpg"; 
+})
 
 
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener("mouseover",function(){
+  image.src= "https://lumiere-a.akamaihd.net/v1/images/pooh_baeb7dc6.jpeg?region=410,0,1229,1229";
+})
 
 // BONUS
 // 7. Try using different event types for your buttons.
+ 
